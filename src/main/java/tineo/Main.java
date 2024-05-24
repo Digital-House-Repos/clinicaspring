541f78c42fe41e94535f0a0cb1c688d5f193d3a8
tineojf <92743762+tineodev@users.noreply.github.com>
2024-05-24 15:10:12 -0500

    Add log4j

--- src/main/java/tineo/Main.java
+++ src/main/java/tineo/Main.java
@@ -1,7 +1,12 @@
 package tineo;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+    private final static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Demo log4j");
         System.out.println("Hello, World!");
     }
 }
