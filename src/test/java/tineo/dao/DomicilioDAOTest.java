37e6dbcf66f4298349b9835bbaf26635ab54682a
tineojf <92743762+tineodev@users.noreply.github.com>
2024-05-25 17:20:25 -0500

    Add testequal test

--- src/test/java/tineo/dao/DomicilioDAOTest.java
+++ src/test/java/tineo/dao/DomicilioDAOTest.java
@@ -21,4 +21,12 @@ public class DomicilioDAOTest {
         DomicilioModel domicilio = domicilioDAO.findById(1);
         Assert.assertNotNull(domicilioDAO.findById(1));
     }
+
+    @Test
+    public void findByIdTest2() {
+        String messageReturn = "Domicilio [domicilioID=1, calle='Calle Falsa', numero=123, localidad='Springfield', provincia='Springfield']";
+        DomicilioDAO domicilioDAO = new DomicilioDAO();
+        DomicilioModel domicilio = domicilioDAO.findById(1);
+        Assert.assertEquals(messageReturn, domicilio.toString());
+    }
 }
