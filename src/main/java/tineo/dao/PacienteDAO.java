646ec6a3a8d3d2ee8ef2fbdafd87ff2375e702c5
tineojf <92743762+tineodev@users.noreply.github.com>
2024-05-25 19:26:57 -0500

    Add FindAll method of DAO

--- src/main/java/tineo/dao/PacienteDAO.java
+++ src/main/java/tineo/dao/PacienteDAO.java
@@ -1,10 +1,15 @@
 package tineo.dao;
 
+import tineo.models.DomicilioModel;
 import tineo.models.PacienteModel;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class PacienteDAO implements IDAO<PacienteModel> {
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(PacienteDAO.class);
 
     @Override
     public PacienteModel create(PacienteModel pacienteModel) {
@@ -18,7 +23,31 @@ public class PacienteDAO implements IDAO<PacienteModel> {
 
     @Override
     public ArrayList<PacienteModel> findAll() {
-        return null;
+        DBConnector connector = DBConnector.getInstance();
+        Connection connection = connector.getConnection();
+        ArrayList<PacienteModel> listDB = new ArrayList<>();
+        String query = "SELECT * FROM PACIENTE;";
+
+        try {
+            ResultSet result = connection.createStatement().executeQuery(query);
+            while (result.next()) {
+                DomicilioDAO domicilioDAO = new DomicilioDAO();
+                DomicilioModel domicilioModel = domicilioDAO.findById(result.getInt("DOMICILIOID"));
+                listDB.add(new PacienteModel(
+                        result.getInt("PACIENTEID"),
+                        result.getString("NOMBRE"),
+                        result.getString("APELLIDO"),
+                        result.getString("DNI"),
+                        result.getDate("FECHAINGRESO").toLocalDate(),
+                        domicilioModel
+                ));
+            }
+            logger.info("GET - Pacientes obtenidos correctamente");
+            return listDB;
+        } catch (SQLException e) {
+            logger.error("GET - Error al obtener los pacientes " + e.getMessage());
+            return null;
+        }
     }
 
     @Override
