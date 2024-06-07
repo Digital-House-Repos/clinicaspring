c67252cb1c750e063db25fc7899f9294612414df
tineojf <92743762+tineodev@users.noreply.github.com>
2024-06-07 18:17:52 -0500

    Delete unuser test & create test to odontologocontroller

---
+++ src/test/java/com/backend/clinica/controller/OdontologoControllerTest.java
@@ -0,0 +1,39 @@
+package com.backend.clinica.controller;
+
+import com.backend.clinica.entity.OdontologoModel;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Objects;
+
+@SpringBootTest
+public class OdontologoControllerTest {
+    @Autowired
+    private OdontologoController odontologoController;
+
+    @Test
+    public void testGetOdontologos() {
+        String expectedMessage = "Odontologos encontrados";
+        String actualMessage = odontologoController.getOdontologos().getBody().getMessage();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void testGetOdontologo() {
+        String expectedMessage = "Odontologo encontrado";
+        String actualMessage = odontologoController.getOdontologo(1L).getBody().getMessage();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void testCreateOdontologo() {
+        OdontologoModel o1 = new OdontologoModel("999", "John", "Doe");
+        Boolean expectedMessage = true;
+        Boolean actualMessage = odontologoController.createOdontologo(o1).getBody().getOk();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+}
+
+
