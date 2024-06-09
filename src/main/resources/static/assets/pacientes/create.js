171029cb00c338f3e5274bf248dab8bf537072a6
tineojf <92743762+tineodev@users.noreply.github.com>
2024-06-09 18:13:00 -0500

    Add create view

---
+++ src/main/resources/static/assets/pacientes/create.js
@@ -0,0 +1,58 @@
+async function dataPacientes(_url, _token = null, _method = 'GET', _body = null) {
+
+  const options = {
+    method: _method,
+    headers: { "Content-Type": "application/json" }
+  };
+
+  if (_token) { options.headers["Authorization"] = "Bearer " + _token; }
+  if (_body) { options.body = JSON.stringify(_body); }
+
+  try {
+    const response = await fetch(_url, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function createPaciente() {
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const dni = document.getElementById('dni').value;
+  const fechaIngreso = document.getElementById('ingreso').value;
+  const domicilio = {
+    calle: document.getElementById('calle').value,
+    numero: document.getElementById('numero').value,
+    localidad: document.getElementById('localidad').value,
+    provincia: document.getElementById('provincia').value
+  };
+
+  const body = {
+    nombre,
+    apellido,
+    dni,
+    fechaIngreso,
+    domicilio
+  };
+
+  console.log(body);
+
+  const URLPacientes = '/pacientes';
+  const data = await dataPacientes(URLPacientes, null, 'POST', body);
+
+  console.log(data);
+
+  if (data.ok) {
+    alert('Paciente creado correctamente');
+  } else {
+    alert('Error al crear paciente' + data.message);
+  }
+}
+
+const formcreate = document.getElementById('form-post-paciente');
+formcreate.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  createPaciente();
+});
